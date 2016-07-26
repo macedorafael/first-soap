c2dd759c5be405ebba738716d15cf5a326aca7bb
rmacedo <rmacedo@BRL0010315.sicpa-net.ads>
2016-07-26 14:11:06 -0300

    Inserindo mais anotações.

--- first-test-soap/src/main/java/com/first/soap/test/ws/WebServiceTest.java
+++ first-test-soap/src/main/java/com/first/soap/test/ws/WebServiceTest.java
@@ -3,6 +3,8 @@ package com.first.soap.test.ws;
 import javax.jws.WebMethod;
 import javax.jws.WebResult;
 import javax.jws.WebService;
+import javax.xml.ws.RequestWrapper;
+import javax.xml.ws.ResponseWrapper;
 
 import com.first.soap.test.dao.ItemDaoImpl;
 import com.first.soap.test.model.ItemList;
@@ -14,6 +16,8 @@ public class WebServiceTest {
 	
 	@WebResult(name="Produtos")// Mudar o nome da lista do resultado para deixar mais expressivo.
 	@WebMethod(operationName="buscarProdutos") //Mudando o nome do metodo no WSDL para o deixar mais expressivo!
+	@ResponseWrapper(localName="PacoteDeItens") //Mundando o nome do pacote de resposta padrão.
+	@RequestWrapper(localName="listaItens")//Mundando o nome do pacote de request padrão.
 	public ItemList getItems(){
 		return dao.getItems();
 	}
